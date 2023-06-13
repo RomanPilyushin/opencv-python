ce90cadee4042015509b3b227b2feea119a93132
Roman <rpilyushin@gmail.com>
2023-06-13 17:18:30 +0300

    Use CascadeClassifier and correct detect face

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import cv2
+#import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-
+"""
 imagePath = 'photo.jpg'
 img = cv2.imread(imagePath)
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -24,3 +25,29 @@ img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 plt.figure(figsize=(20,10))
 plt.imshow(img_rgb)
 plt.axis('off')
+
+"""
+
+# Read image from your local file system
+original_image = cv.imread('img.png')
+
+# Convert color image to grayscale for Viola-Jones
+grayscale_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+
+# Load the classifier and create a cascade object for face detection
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
+
+detected_faces = face_cascade.detectMultiScale(grayscale_image)
+
+for (column, row, width, height) in detected_faces:
+    cv.rectangle(
+        original_image,
+        (column, row),
+        (column + width, row + height),
+        (0, 255, 0),
+        2
+    )
+
+cv.imshow('Image', original_image)
+cv.waitKey(0)
+cv.destroyAllWindows()
